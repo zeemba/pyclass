60fdc37110c5d61a978375f5fb4b1b3618213e7b
Azeem <abdulazeemanimasaun@gmail.com>
2018-10-30 15:02:51 +0000

    update

---
+++ fortwo.py
@@ -0,0 +1,4 @@
+number = 0
+for i in range(0,101,2):
+    number = number + i
+    print(str(number) + ' and ' + str(i))
---
+++ while.py
@@ -0,0 +1,4 @@
+spam = 0
+while spam < 5:
+    print('Hello')
+    spam = spam + 1
