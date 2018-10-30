e8f68ac67bc43b8c7b774bc3981b4d61760330ab
Azeem <abdulazeemanimasaun@gmail.com>
2018-10-30 13:45:37 +0000

    updated flow.py

---
+++ flow.py
@@ -0,0 +1,10 @@
+name = 'Oyin'
+if name == 'Azeem':
+    print('Hi ' + name)
+elif name == 'Oyin':
+    print('Hey baby!')
+else:
+    print('Oh what is your name then?')
+    myName = input()
+    print('Great!, Hi ' + myName)
+print('Done')
